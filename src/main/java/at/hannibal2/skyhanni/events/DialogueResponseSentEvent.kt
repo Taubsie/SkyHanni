22c89ff5eec3daf2ffa7745401a43cb93eb64eab
taubsie <taubsie@dungeon-hub.net>
2026-09-24 01:18:26 +0200

    Improved the documentation on the event.

--- src/main/java/at/hannibal2/skyhanni/events/DialogueResponseSentEvent.kt
+++ src/main/java/at/hannibal2/skyhanni/events/DialogueResponseSentEvent.kt
@@ -1,6 +1,14 @@
 package at.hannibal2.skyhanni.events
 
 import at.hannibal2.skyhanni.api.event.SkyHanniEvent
+import at.hannibal2.skyhanni.skyhannimodule.PrimaryFunction
 import net.minecraft.network.protocol.common.ServerboundCustomClickActionPacket
 
+/**
+ * Fired when the player clicks on any chat component with a custom click action dedicated to skyblock dialogue responses.
+ * This is handled by [at.hannibal2.skyhanni.api.NpcApi] by filtering the outgoing packets.
+ *
+ * @param packet The actual packet that is sent to the server.
+ */
+@PrimaryFunction("onDialogueResponseSent")
 class DialogueResponseSentEvent(val packet: ServerboundCustomClickActionPacket) : SkyHanniEvent()
